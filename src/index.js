2b744486a39696cf3f6741ff40d2d073283c3046
Laura Martins <lauraalvesmartins04@gmail.com>
2022-09-01 16:28:20 -0300

    projeto finalizado

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import {createStore, applyMiddleware, compose } from 'redux'
 import reducer from './reducers'
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
 const logger = store => next => action => {
   console.group(action.type)
